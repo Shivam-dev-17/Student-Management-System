8bac71b27db5151d564e385aa71729521ae6a3fc
Shivam-dev-17 <singhshivam1703@gmail.com>
2025-11-24 22:29:52 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.scanner;
+import model.Student;
+import services.StudentService;
+
+public class Main{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    StudentService service = new StudentService();
+
+    while(true){
+      System.out.println("\n--- VITyarthi Student Management ---");
+      System.out.println("1. Add Student");
+      System.out.println("2. View Students");
+      System.out.println("3. Search Student by ID");
+      System.out.println("4. Exit");
+      System.out.print("Enter choice: ");
+
+      int choice= sc.nextInt();
+      sc.nextLine();
+
+    switch(choice){
+      case 1:
+        System.out.print("Enter ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Course: ");
+        String course = sc.nextLine();
+
+        service.addStudent(new Student(id, name, course));
+        System.out.println("Student added.\n");
+        break;
+      case 2:
+        service.showAll();
+        break;
+      case 3:
+        System.out.print("Enter ID to search: ");
+        int sid = sc.nextInt();
+        service.searchById(sid);
+        break;
+      case 4:
+        System.out.println("Exiting....");
+        System.exit(0);
+
+      default:
+        System.out.println("Invalid choice.\n");
+            }
+        }
+    }
+}
